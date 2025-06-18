1bcd0436e447369ce6455ab02e00a22e8a5ce3fd
Jon-work <jonathansalazar422@gmail.com>
2025-06-18 16:33:40 -0600

    feat: add examples about ES Modules

--- 02-javascript/05-modulos/modulos.js
+++ 02-javascript/05-modulos/modulos.js
@@ -1,1 +1,17 @@
-console.log("Archivo módulos");
+//import { nombre, saludar } from "./archivo.js";
+
+//import funcionDefault from "./archivo.js";
+// Importar todo
+//import * as Utils from "./archivo.js";
+//Utils.sumar(1, 2);
+
+// Export nombrado
+export const edad = 30;
+
+// Export por defecto
+export default function sumar(a, b) {
+  return a + b;
+}
+
+// Todo desde un solo objeto
+export { edad, sumar };
